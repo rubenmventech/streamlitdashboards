67d6f1cf08098bb009efaf1e36eede0f09e315e8
rubenmventech <ruben.vanwyk@mventech.com>
2022-11-24 15:22:55 +0200

    add animation

--- hellodiana.py
+++ hellodiana.py
@@ -1,3 +1,16 @@
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+from PIL import Image
+
+def load_lottieurl(url):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
 
 st.write("Hello Diana")
+
+lottie_love = load_lottieurl("https://assets7.lottiefiles.com/packages/lf20_jvxwtdtp.json")
+
+st_lottie(lottie_love, height=300, key="coding")
