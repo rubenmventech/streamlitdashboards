be08a441a15f333992add086475b65ad2257e7ab
rubenmventech <ruben.vanwyk@mventech.com>
2022-11-24 13:57:09 +0200

    Update testerapp.py

--- testerapp.py
+++ testerapp.py
@@ -1,4 +1,13 @@
-import pandas as pd
-import streamlit as st
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
 
-st.write("Hello from Streamlit")
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
+
+df
