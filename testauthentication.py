2ed58ed7524863623ac357c26d76028d869d18c9
rubenmventech <ruben.vanwyk@mventech.com>
2022-12-09 10:40:12 +0200

    trying to build new authentication page

---
+++ testauthentication.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import streamlit_authenticator as stauth
+import yaml
+
+with open('usercredentials.yaml') as file:
+    config = yaml.safe_load(file)
+
+authenticator = stauth.Authenticate(
+    config['credentials'],
+    config['cookie']['name'],
+    config['cookie']['key'],
+    config['cookie']['expiry_days'],
+    config['preauthorized']
+)
+
+name, authentication_status, username = authenticator.login('Login', 'main')
