48553e34016fdc1f8dbbdc258b30a217f14c114c
rubenmventech <ruben.vanwyk@mventech.com>
2022-12-09 10:44:57 +0200

    update to actually allow login mechanic

--- testauthentication.py
+++ testauthentication.py
@@ -14,3 +14,12 @@ authenticator = stauth.Authenticate(
 )
 
 name, authentication_status, username = authenticator.login('Login', 'main')
+
+if authentication_status:
+    authenticator.logout('Logout', 'main')
+    st.write(f'Welcome *{name}*')
+    st.title('Some content')
+elif authentication_status == False:
+    st.error('Username/password is incorrect')
+elif authentication_status == None:
+    st.warning('Please enter your username and password')
